17bebf0e7463990a3416c47b8f39cf6da3c692c7
romulo.milani <81986766+romulo-milani@users.noreply.github.com>
2025-01-09 22:52:58 -0300

    adicionado função que mostra o vencedor de 5 rodadas

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ let scissors = 2;
 let humanScore = 0;
 let computerScore = 0;
 let round = 1;
+let winner = "";
 let rockButton = document.querySelector("#rock");
 let paperButton = document.querySelector("#paper");
 let scissorsButton = document.querySelector("#scissors");
 let playerOptions = document.querySelector(".playerOptions");
+let playerScoreSpan = document.querySelector(".player-score");
+let computerScoreSpan = document.querySelector(".computer-score")
+
+playerScoreSpan.textContent = `Player score: ${humanScore}`;
+computerScoreSpan.textContent = `Computer score: ${computerScore}`;
 
 playerOptions.addEventListener("click", playRound);
 
@@ -21,6 +27,16 @@ function getComputerChoice() {
 function getHumanChoice(e) {
     let humanChoice = e.target.textContent;
     return humanChoice;    
+}
+
+function endgame() {
+    document.querySelector(".player-choice").remove();
+    document.querySelector(".computer-choice").remove();
+    document.querySelector(".winner").remove();
+
+    let h1 = document.createElement('h1');
+    h1.textContent = `The winner is: ${winner}`;
+    document.querySelector("body").appendChild(h1);
 }
 
 function playRound(e) {
@@ -38,43 +54,73 @@ function playRound(e) {
         switch(humanChoice) {
             case "rock": 
             winnerSpan.textContent = "Tie!";
+            humanScore += 1;
+            computerScore += 1;
             break;
 
             case "paper": 
             winnerSpan.textContent = "Player wins!";
+            humanScore += 1;
             break;
 
             case "scissors": 
             winnerSpan.textContent = "Computer wins!";
+            computerScore += 1;
             break;
         }        
     } else if (computerChoice == "paper") {
         switch(humanChoice) {
             case "rock": 
             winnerSpan.textContent = "Computer wins!";
+            computerScore += 1;
             break;
 
             case "paper": 
             winnerSpan.textContent = "Tie!";
+            computerScore += 1;
+            humanScore += 1;
             break;
 
             case "scissors": 
             winnerSpan.textContent = "Player wins!";
+            humanScore += 1;
             break;
         } 
     } else if (computerChoice == "scissors") {
         switch(humanChoice) {
             case "rock": 
             winnerSpan.textContent = "Player wins!";
+            humanScore += 1;
             break;
 
             case "paper": 
             winnerSpan.textContent = "Computer wins!";
+            computerScore += 1;
             break;
 
             case "scissors": 
             winnerSpan.textContent = "Tie!";
+            humanScore += 1;
+            computerScore += 1;
             break;
         } 
+    }
+
+    playerScoreSpan.textContent = `Player score: ${humanScore}`;
+    computerScoreSpan.textContent = `Computer score: ${computerScore}`;
+    
+    if (humanScore == 5 && computerScore == 5) {
+        winner = "tie";
+        endgame();
+    }
+
+    if (humanScore == 5 && computerScore < 5) {
+        winner = "player";
+        endgame();
+    }
+
+    if (humanScore < 5 && computerScore == 5) {
+        winner = "computer";
+        endgame();
     }
 }
